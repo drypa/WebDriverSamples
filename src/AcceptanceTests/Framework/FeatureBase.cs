3305ba3bcc3f88a1a714669360c167c5bfb66da8
DRypa <dr.drypa.k@mail.ru>
2016-05-27 18:34:40 +0300

    init

---
+++ src/AcceptanceTests/Framework/FeatureBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace AcceptanceTests.Framework
+{
+    public abstract class FeatureBase
+    {
+        protected void WaitDocumentLoaded(IWebDriver IeDriver)
+        {
+            ((IWait<IWebDriver>)new WebDriverWait(IeDriver, TimeSpan.FromSeconds(30))).Until(JsIsDocumentLoaded);
+        }
+
+        private bool JsIsDocumentLoaded(IWebDriver driver)
+        {
+            try
+            {
+                return ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState;").Equals("complete");
+            }
+            catch (InvalidOperationException)
+            { }
+            return false;
+        }
+    }
+}
---
+++ src/AcceptanceTests/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AcceptanceTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
