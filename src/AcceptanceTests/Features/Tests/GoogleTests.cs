2a929b96ad007005f42128f645fb7007f3f34bcc
DRypa <dr.drypa.k@mail.ru>
2016-07-21 11:13:24 +0300

    nunit + specflow on several browsers

---
+++ src/AcceptanceTests/Features/Tests/GoogleTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace AcceptanceTests.Features.Tests
+{
+    [TestFixture(typeof(InternetExplorerDriver))]
+    [TestFixture(typeof(FirefoxDriver))]
+    [TestFixture(typeof(ChromeDriver))]
+    public class GoogleTests<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void CreateDriver()
+        {
+            driver = new TWebDriver();
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            Assert.IsNotNull(driver);
+            driver.Navigate().GoToUrl("http://www.google.com/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
